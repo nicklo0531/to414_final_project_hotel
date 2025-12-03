c15150640990b2412d4bad87e379c504f2d7b3a6
nicklo <nicklo@uw.edu>
2025-12-03 00:19:45 -0500

    update random forest

--- models/RandomForest.R
+++ models/RandomForest.R
@@ -3,7 +3,7 @@ library(caret)
 df_train <- read.csv("../data/hotel_train.csv")
 df_test <- read.csv("../data/hotel_test.csv")
 
-rf_model <- randomForest(type='classification', as.factor(is_canceled) ~ ., data = df_train, ntree=500, weights=NULL)
+rf_model <- randomForest(type='classification', as.factor(is_canceled) ~ ., data = df_train, ntree=800, weights=NULL)
 rf_pred <- predict(rf_model, df_test)
 
 rf_cm <- confusionMatrix(as.factor(rf_pred), as.factor(df_test$is_canceled), positive = "1")
